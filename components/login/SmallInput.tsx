2856c483efe0d9326ed4cc2d6471e00e187a2883
Moaz Ayman <moaz.ayman.mahmoud@gmail.com>
2025-02-05 00:50:57 +0200

    Update focus style in SmallInput component for improved accessibility

--- components/login/SmallInput.tsx
+++ components/login/SmallInput.tsx
@@ -12,7 +12,7 @@ const SmallInput = ({
     <View className="mt-2 sm:mt-0">
       <Text className="text-[#363E4C] font-Roboto-Medium text-lg">{label}</Text>
       <TextInput
-        className="p-4 border-2 border-[#EDEDED] rounded-lg font-Roboto-Thin focus:border-[#147E93] min-w-[180px] placeholder:text-[#363E4C]"
+        className="p-4 border-2 border-[#EDEDED] rounded-lg font-Roboto-Thin focus:outline-[#147E93] min-w-[180px] placeholder:text-[#363E4C]"
         placeholder={placeholder}
       />
     </View>
