4418718ca9205c3085408eb54cbacc5b5cc0d2b4
Moaz Ayman <moaz.ayman.mahmoud@gmail.com>
2025-02-08 17:24:50 +0200

    Implement login functionality with error handling and loading state; update input components for better user experience

--- components/login/Input.tsx
+++ components/login/Input.tsx
@@ -1,22 +1,32 @@
 import { View, Text, TextInput } from 'react-native';
-import React from 'react';
 
 const Input = ({
   label,
   placeholder,
   isPassword,
+  value,
+  onChangeText,
+  error,
+  onFocus,
 }: {
   label: string;
   placeholder: string;
   isPassword: boolean;
+  value: string;
+  onChangeText: (text: string) => void;
+  error?: boolean;
+  onFocus?: () => void;
 }) => {
   return (
     <View className="w-full mt-2">
       <Text className="text-[#363E4C] font-Roboto-Medium text-lg">{label}</Text>
       <TextInput
-        className="p-4 border-2 border-[#EDEDED] rounded-lg font-Roboto-Thin focus:outline-[#147E93] placeholder:text-[#363E4C]"
+        className={`p-4 border-2 ${error ? 'border-[#FF5757]' : 'border-[#EDEDED]'} rounded-lg font-Roboto-Thin focus:outline-[#147E93] placeholder:text-[#363E4C]`}
         placeholder={placeholder}
         secureTextEntry={isPassword}
+        onChangeText={onChangeText}
+        value={value}
+        onFocus={onFocus}
       />
     </View>
   );
