4639188acb9c372930da7bb163082f0b2e44f29d
Moaz Ayman <moaz.ayman.mahmoud@gmail.com>
2025-04-11 17:32:38 +0200

    Update BottomNavigation component and enhance responsiveness of the services pages

--- app/(tabs)/profile/my-services.tsx
+++ app/(tabs)/profile/my-services.tsx
@@ -1,8 +1,16 @@
-import { View, Text, Image, FlatList, TouchableOpacity } from 'react-native'
-import React from 'react'
-import { Ionicons } from "@expo/vector-icons"
-import { StarRatingDisplay } from 'react-native-star-rating-widget'
-import { useRouter } from 'expo-router'
+import {
+  View,
+  Text,
+  Image,
+  FlatList,
+  TouchableOpacity,
+  useWindowDimensions,
+} from 'react-native';
+import React from 'react';
+import { Ionicons } from '@expo/vector-icons';
+import { Rating } from 'react-native-ratings';
+
+import { useRouter } from 'expo-router';
 
 const servicesData = [
   {
@@ -47,13 +55,18 @@ const servicesData = [
   },
 ];
 const MyServices = () => {
-    const router = useRouter()
+  const router = useRouter();
+
+  const { width: screenWidth } = useWindowDimensions();
+
+  const imageSize = screenWidth < 375 ? 45 : screenWidth < 768 ? 60 : 75;
+
   return (
     <View className="flex-1 bg-[#F9F9F9]">
       {/* Header */}
       <View className="flex-row items-center justify-center px-4 py-4 relative mt-6">
-        <TouchableOpacity 
-          onPress={() => router.push('/profile/me')} 
+        <TouchableOpacity
+          onPress={() => router.push('/profile/me')}
           className="absolute left-4 w-6 h-6"
         >
           <Image
@@ -62,73 +75,87 @@ const MyServices = () => {
             resizeMode="contain"
           />
         </TouchableOpacity>
-        
-        <Text className="text-center text-[#030B19] font-Roboto-SemiBold text-xl"> My Services</Text>
+
+        <Text className="text-center text-[#030B19] font-Roboto-SemiBold text-xl">
+          {' '}
+          My Services
+        </Text>
 
         <View className="absolute right-4">
           <View className="relative">
             <Ionicons name="notifications-outline" size={24} color="#030B19" />
-            <View className="absolute -top-2 -right-2 bg-[#FDBC10] rounded-full w-5 h-5 items-center justify-center">
+            <View className="absolute -top-2 -right-2 bg-red-500 rounded-full w-5 h-5 items-center justify-center">
               <Text className="text-white text-xs font-bold">4</Text>
             </View>
           </View>
         </View>
       </View>
-      
+
       {/* Services List */}
-      <View className="flex-1 bg-[#FFFFFF] rounded-2xl mx-4 mt-2 shadow-md">
-        <FlatList 
+      <View className="bg-[#FFFFFF] rounded-2xl mx-2 xs:mx-4 xs:px-4 mb-5 px-2  flex-1">
+        <FlatList
           data={servicesData}
           keyExtractor={(item) => item.id}
-          className="px-4 pb-5"
           renderItem={({ item }) => (
             <TouchableOpacity
-              onPress={() => router.push(`/services/my-services`)}
-              className="flex-row items-center justify-between bg-[#FFFFFF] py-3 px-3 "
+              onPress={() => router.push(`/services/providerServices`)}
+              className="flex-row py-3 w-full items-center"
             >
-              
-              <View className="flex-row items-center">
-                <Image
-                  source={item.image}
-                  className="rounded-full mr-3"
-                  style={{
-                    width: 60,
-                    height: 60,
-                    maxWidth: 70, 
-                    maxHeight: 70, 
-                  }}
-                  resizeMode="cover" 
-                />
-                <View>
-                  <Text className="text-[#030B19] font-bold text-base">{item.title}</Text>
-                  <Text className="text-gray-600 text-sm">{item.provider}</Text>
-                  <View className="flex-row items-center mt-1">
-                    <Text className="text-sm text-[#030B19] mr-1">{item.rating}</Text>
-                    <StarRatingDisplay rating={item.rating} starSize={14} starStyle={{ marginRight: 1 }} />
-                  </View>
+              <Image
+                source={item.image}
+                className="rounded-full mr-3"
+                style={{
+                  width: imageSize,
+                  height: imageSize,
+                }}
+                resizeMode="cover"
+              />
+              <View className="flex-1 ">
+                <Text className="text-[#030B19] font-bold text-sm xs:text-base">
+                  {item.title}
+                </Text>
+                <View className="flex-row items-center justify-between">
+                  <Text className="text-gray-600 text-xs xs:text-sm flex-1 pr-2">
+                    {item.provider}
+                  </Text>
+                  <Ionicons name="chevron-forward" size={20} color="#030B19" />
                 </View>
-              </View>
-          
-              <View className="items-end">
-                <Text className="text-[#030B19] font-semibold text-sm">{item.price}</Text>
-                <Ionicons name="chevron-forward" size={20} color="#030B19" />
+
+                <View className="flex-row items-center justify-between">
+                  <View className="flex-row items-center gap-1">
+                    <Text className="text-sm text-[#030B19] mr-1">
+                      {item.rating}
+                    </Text>
+                    <Rating
+                      readonly
+                      startingValue={item.rating}
+                      imageSize={10}
+                    />
+                  </View>
+                  <Text className="text-[#030B19] font-semibold text-xs xs:text-sm">
+                    {item.price}
+                  </Text>
+                </View>
               </View>
             </TouchableOpacity>
           )}
+          showsVerticalScrollIndicator={false}
         />
       </View>
 
       {/* Footer Button */}
       <View className="mt-4 px-4 mb-3">
-        <TouchableOpacity 
+        <TouchableOpacity
           onPress={() => router.push('/services/add-service')}
           className="bg-[#FDBC10] rounded-xl items-center justify-center w-[156px] h-[50px] self-end shadow-lg"
         >
-          <Text className="text-[#030B19] font-Roboto-Medium ">Add new service</Text>
+          <Text className="text-[#030B19] font-Roboto-Medium ">
+            Add new service
+          </Text>
         </TouchableOpacity>
       </View>
     </View>
-  )
-}
+  );
+};
 
-export default MyServices
+export default MyServices;
--- app/(tabs)/services/index.tsx
+++ app/(tabs)/services/index.tsx
@@ -1,12 +1,15 @@
 import {
   View,
   Text,
+  TextInput,
   Image,
+  SafeAreaView,
   FlatList,
-  TouchableOpacity,
   useWindowDimensions,
+  TouchableOpacity,
 } from 'react-native';
-import React from 'react';
+import React, { useState } from 'react';
+import NotificationIcon from '@/assets/icons/Notification';
 import { Ionicons } from '@expo/vector-icons';
 import { Rating } from 'react-native-ratings';
 import { useRouter } from 'expo-router';
@@ -54,38 +57,79 @@ const servicesData = [
   },
 ];
 
-const ProviderServices = () => {
+const AllServices = () => {
   const router = useRouter();
   const { width: screenWidth } = useWindowDimensions();
+
+  // Calculate image size based on screen width
   const imageSize = screenWidth < 375 ? 45 : screenWidth < 768 ? 60 : 75;
 
+  const [searchQuery, setSearchQuery] = useState('');
+  const [filteredServices, setFilteredServices] = useState(servicesData);
+
+  // Search function
+  const handleSearch = (text: string) => {
+    setSearchQuery(text);
+    if (text.trim() === '') {
+      setFilteredServices(servicesData);
+    } else {
+      const filtered = servicesData.filter((service) =>
+        service.title.toLowerCase().includes(text.toLowerCase()),
+      );
+      setFilteredServices(filtered);
+    }
+  };
+
   return (
-    <View className="flex-1 bg-[#F9F9F9]">
+    <SafeAreaView className="flex-1 bg-[#F9F9F9]">
       {/* Header */}
       <View className="flex-row items-center justify-center px-4 py-4 relative mt-6">
-        <TouchableOpacity
-          onPress={() => router.push('/services/all-services')}
-          className="absolute left-4 w-6 h-6"
-        >
-          <Image
-            source={require('@/assets/images/blackArrow.png')}
-            className="w-full h-full"
-            resizeMode="contain"
-          />
-        </TouchableOpacity>
-
-        <Text className="text-center text-[#030B19] font-Roboto-SemiBold text-lg">
-          {' '}
-          Services by (Provider Name){' '}
+        <Text className="text-center text-[#030B19] font-Roboto-SemiBold text-2xl">
+          Services
         </Text>
-
-        <View className="absolute right-4">
-          <View className="relative">
-            <Ionicons name="notifications-outline" size={24} color="#030B19" />
-            <View className="absolute -top-2 -right-2 bg-red-500 rounded-full w-5 h-5 items-center justify-center">
-              <Text className="text-white text-xs font-bold">4</Text>
+        <View style={{ position: 'absolute', right: 16, marginBottom: 23 }}>
+          <View className="absolute right-4">
+            <View className="relative ">
+              <NotificationIcon />
+              <View className="absolute -top-2 -right-2 bg-red-500 rounded-full w-5 h-5 items-center justify-center">
+                <Text className="text-white text-xs font-bold">4</Text>
+              </View>
             </View>
           </View>
+        </View>
+      </View>
+
+      {/* Search & Filter */}
+      <View className="px-4 py-2 flex-row items-center justify-between mb-2">
+        <View className="flex-row items-center bg-white rounded-lg px-2 py-2 flex-1 mr-3 h-10">
+          <Ionicons
+            name="search"
+            size={20}
+            color="#030B19"
+            style={{ paddingLeft: 4 }}
+          />
+          <SafeAreaView style={{ flex: 1, marginHorizontal: 10 }}>
+            <TextInput
+              placeholder="Search..."
+              clearButtonMode="always"
+              value={searchQuery}
+              onChangeText={handleSearch}
+              className="flex-1 text-base text-[#666B73]"
+              placeholderTextColor="#666B73"
+              style={{
+                paddingHorizontal: 5,
+                paddingVertical: 5,
+                borderWidth: 0,
+              }}
+            />
+          </SafeAreaView>
+        </View>
+
+        <View className="w-10 h-10 bg-white rounded-lg justify-center items-center">
+          <Image
+            source={require('@/assets/images/filter.png')}
+            resizeMode="contain"
+          />
         </View>
       </View>
 
@@ -140,8 +184,8 @@ const ProviderServices = () => {
           showsVerticalScrollIndicator={false}
         />
       </View>
-    </View>
+    </SafeAreaView>
   );
 };
 
-export default ProviderServices;
+export default AllServices;
