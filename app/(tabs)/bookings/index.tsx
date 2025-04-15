3635a826b33a24f7c2b629215b2fc9afb6faa260
Moaz Ayman <moaz.ayman.mahmoud@gmail.com>
2025-04-15 21:32:02 +0200

    fetch bookings data from API and update bookings list; refactor booking item rendering

--- app/(tabs)/bookings/index.tsx
+++ app/(tabs)/bookings/index.tsx
@@ -1,144 +1,157 @@
-import React from 'react';
-import { View, Text, Image, FlatList, TouchableOpacity } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {
+  View,
+  Text,
+  Image,
+  FlatList,
+  TouchableOpacity,
+  ActivityIndicator,
+} from 'react-native';
+import { router } from 'expo-router';
 import Header from '@/components/Header';
 import { Ionicons } from '@expo/vector-icons';
+import ApiService from '@/constants/ApiService';
+import { API_ENDPOINTS } from '@/constants/ApiConfig';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import BookingText from '@/components/BookingText';
 
-const bookingsData = [
-  {
-    id: '1',
-    name: 'Ali Elnaggar',
-    service: 'Bedroom',
-    price: '10.500$',
-    cimg: require('@/assets/images/Profile.png'),
-    status: 'open',
-  },
-  {
-    id: '2',
-    name: 'Ali Elnaggar',
-    service: 'Arm Chair',
-    price: '10.500$',
-    cimg: require('@/assets/images/Profile.png'),
-    status: 'closed',
-  },
-  {
-    id: '3',
-    name: 'Ali Elnaggar',
-    service: 'Tuffet',
-    price: '10.500$',
-    cimg: require('@/assets/images/Profile.png'),
-    status: 'open',
-  },
-  {
-    id: '4',
-    name: 'Ali Elnaggar',
-    service: 'Plan chest',
-    price: '10.500$',
-    cimg: require('@/assets/images/Profile.png'),
-    status: 'closed',
-  },
-  {
-    id: '5',
-    name: 'Ali Elnaggar',
-    service: 'Dinette (group)',
-    price: '10.500$',
-    cimg: require('@/assets/images/Profile.png'),
-    status: 'open',
-  },
-  {
-    id: '6',
-    name: 'Ali Elnaggar',
-    service: 'Bar cabinet',
-    price: '10.500$',
-    cimg: require('@/assets/images/Profile.png'),
-    status: 'open',
-  },
-  {
-    id: '7',
-    name: 'Ali Elnaggar',
-    service: 'Bar cabinet',
-    price: '10.500$',
-    cimg: require('@/assets/images/Profile.png'),
-    status: 'open',
-  },
-  {
-    id: '8',
-    name: 'Ali Elnaggar',
-    service: 'Bar cabinet',
-    price: '10.500$',
-    cimg: require('@/assets/images/Profile.png'),
-    status: 'open',
-  },
-];
+interface BookingItem {
+  _id: string;
+  service: {
+    service_name: string;
+    base_price: string;
+    service_provider: {
+      full_name: string;
+      profile_image: string;
+    };
+  };
+}
 
-const customerBookings = () => {
-  const renderBookingItem = ({ item }: { item: any }) => (
-    <TouchableOpacity
-    //   onPress={() => router.push('/bookings/customerBookings')} // Navigate to chat
-    >
-      <View
-        className={`flex-row items-center justify-between ${item.id & 1 ? 'bg-white' : 'bg-[#F9F9F9]'} px-4 py-3`}
-      >
-        <View className="flex-row items-center">
-          <Image
-            source={item.cimg}
-            className="w-10 h-10 rounded-full"
-            resizeMode="cover"
-          />
-          <View className="ml-3">
-            <Text className="text-sm font-Roboto-Medium text-gray-900">
-              {item.name}
+const Booking = () => {
+  const [bookings, setBookings] = useState<BookingItem[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [role, setRole] = useState<string | null>(null);
+
+  useEffect(() => {
+    // Fetch bookings data from the API
+    const fetchBookings = async () => {
+      try {
+        setLoading(true);
+        const response: any = await ApiService.get(API_ENDPOINTS.GET_BOOKINGS);
+        const userData = await AsyncStorage.getItem('user');
+        if (userData) {
+          const parsedUser = JSON.parse(userData);
+          setRole(parsedUser.role);
+        } else {
+          router.push('/(otp)/customer/login');
+        }
+        setBookings(response.data.data || []);
+      } catch (error) {
+        console.error('Error fetching bookings:', error);
+        setLoading(false);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchBookings();
+  }, []);
+
+  const renderBookingItem = ({
+    item,
+    index,
+  }: {
+    item: BookingItem;
+    index: number;
+  }) => {
+    const service = item.service;
+    const providerName = service.service_provider?.full_name;
+    const providerImage = service.service_provider?.profile_image;
+    const serviceName = service.service_name;
+    const servicePrice = service.base_price;
+
+    return (
+      <TouchableOpacity>
+        <View
+          className={`flex-row items-center justify-between ${index % 2 === 0 ? 'bg-white' : 'bg-[#F9F9F9]'} px-4 py-3`}
+        >
+          <View className="flex-row items-center">
+            <Image
+              source={{ uri: providerImage }}
+              className="w-10 h-10 rounded-full"
+              resizeMode="cover"
+            />
+            <View className="ml-3">
+              <Text className="text-sm font-Roboto-Medium text-gray-900">
+                {providerName}
+              </Text>
+              <Text className="text-sm text-gray-500">{serviceName}</Text>
+            </View>
+          </View>
+
+          <View className="items-end mb-4">
+            <Text className="text-xs font-Roboto text-gray-900">
+              {servicePrice} EGP
             </Text>
-            <Text className="text-sm text-gray-500">{item.service}</Text>
           </View>
         </View>
+      </TouchableOpacity>
+    );
+  };
 
-        <View className="items-end mb-4">
-          <Text className="text-xs font-Roboto text-gray-900">
-            {item.price}
-          </Text>
-          {item.status === 'open' && (
-            <View className="h-2 w-2 bg-blue-500 rounded-full mt-2" />
-          )}
-        </View>
+  if (loading) {
+    return (
+      <View className="flex items-center justify-center h-screen">
+        <ActivityIndicator size="large" color="#0000ff" />
       </View>
-    </TouchableOpacity>
-  );
+    );
+  }
 
   return (
-    <View className="flex-1 bg-gray-50">
-      {/*  Header */}
-      <Header
-        title="Bookings"
-        textSize="text-xl"
-        backRoute="/bookings/bookings"
-        notificationsCount={4}
-      />
+    <>
+      {bookings.length === 0 ? (
+        <View className="flex-1 items-center justify-center">
+          <BookingText />
+        </View>
+      ) : (
+        <View className="flex-1 bg-gray-100">
+          {/* Header */}
+          <Header
+            title="Bookings"
+            textSize="text-xl"
+            backRoute="/bookings/bookings"
+            notificationsCount={4}
+          />
 
-      {/* Bookings List */}
-      <View
-        className="flex-1 bg-[#FFFFFF] rounded-2xl mx-4 shadow-md"
-        style={{ marginBottom: 30 }}
-      >
-        <FlatList
-          data={bookingsData}
-          keyExtractor={(item) => item.id}
-          renderItem={renderBookingItem}
-          contentContainerStyle={{ paddingBottom: 16 }}
-          style={{
-            borderRadius: 20,
-          }}
-          showsVerticalScrollIndicator={false}
-        />
-      </View>
+          {/* Bookings List */}
+          <View
+            className="flex-1 bg-[#FFFFFF] rounded-2xl mx-4"
+            style={{ marginBottom: 30 }}
+          >
+            <FlatList
+              data={bookings}
+              keyExtractor={(item, index) => item._id || `booking-${index}`}
+              renderItem={renderBookingItem}
+              contentContainerStyle={{ paddingBottom: 16 }}
+              style={{
+                borderRadius: 20,
+              }}
+              showsVerticalScrollIndicator={false}
+            />
+          </View>
 
-      <TouchableOpacity
-        // onPress={() => router.push('/bookings/addBooking')} // Navigate to the Add Booking screen
-        className="bg-[#147E93] h-14 w-14 rounded-full items-center justify-center shadow-lg mb-4 mr-4 self-end"
-      >
-        <Ionicons name="add" size={28} color="#FFFFFF" />
-      </TouchableOpacity>
-    </View>
+          {role === 'customer' ? (
+            <TouchableOpacity
+              onPress={() => router.push('/bookings')}
+              className="bg-[#147E93] h-14 w-14 rounded-full items-center justify-center shadow-lg mb-4 mr-4 self-end"
+            >
+              <Ionicons name="add" size={28} color="#FFFFFF" />
+            </TouchableOpacity>
+          ) : null}
+        </View>
+      )}
+    </>
   );
 };
 
-export default customerBookings;
+export default Booking;
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,31 +1,31 @@
 import React from 'react';
 import { View, Text, Image, TouchableOpacity } from 'react-native';
 import { useRouter } from 'expo-router';
-import NotificationIcon from '@/assets/icons/Notification'; 
+import NotificationIcon from '@/assets/icons/Notification';
 
 interface HeaderProps {
-  title: string; 
-  textSize?: string; 
-  backRoute?: string; 
-  notificationsCount?: number; 
-  showBackButton?: boolean; 
+  title: string;
+  textSize?: string;
+  backRoute?: string;
+  notificationsCount?: number;
+  showBackButton?: boolean;
 }
 
 const Header: React.FC<HeaderProps> = ({
   title,
-  textSize = 'text-xl', 
-  backRoute = '', 
+  textSize = 'text-xl',
+  backRoute = '',
   notificationsCount = 0,
   showBackButton = true,
 }) => {
-  const router = useRouter(); 
+  const router = useRouter();
 
   return (
     <View className="flex-row items-center justify-center px-4 py-4 relative mt-6">
       {showBackButton && (
-        <TouchableOpacity 
-          onPress={() => router.push(backRoute as any)} 
-          className="absolute left-4 w-6 h-6"
+        <TouchableOpacity
+          onPress={() => router.push(backRoute as any)}
+          className="absolute left-4 w-6 h-4"
         >
           <Image
             source={require('@/assets/images/leftArrow.png')}
@@ -34,18 +34,22 @@ const Header: React.FC<HeaderProps> = ({
           />
         </TouchableOpacity>
       )}
-      
-      <Text className={`text-center text-[#030B19] font-Roboto-SemiBold ${textSize}`}>
+
+      <Text
+        className={`text-center text-[#030B19] font-Roboto-SemiBold ${textSize}`}
+      >
         {title}
       </Text>
 
       <View className="absolute right-4">
         <TouchableOpacity onPress={() => router.push('/profile/notification')}>
           <View className="relative">
-            <NotificationIcon color="#030B19" /> 
+            <NotificationIcon color="#030B19" />
             {notificationsCount > 0 && (
-              <View className="absolute -top-2 -right-2 bg-[#FDBC10] rounded-full w-5 h-5 items-center justify-center">
-                <Text className="text-white text-xs font-bold">{notificationsCount}</Text>
+              <View className="absolute -top-2 -right-2 bg-red-500 rounded-full w-5 h-5 items-center justify-center">
+                <Text className="text-white text-xs font-bold">
+                  {notificationsCount}
+                </Text>
               </View>
             )}
           </View>
