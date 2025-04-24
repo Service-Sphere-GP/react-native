ca8c17339271a14b8b7b6c77b23ed1666abd8601
Moaz Ayman <moaz.ayman.mahmoud@gmail.com>
2025-04-24 19:30:46 +0200

    Refactor Rating component to improve star rendering logic and handle half-star ratings

--- components/ui/Rating.tsx
+++ components/ui/Rating.tsx
@@ -5,18 +5,26 @@ import { Ionicons } from '@expo/vector-icons';
 const Rating = ({ value }: { value: number | undefined }) => {
   return (
     <View className="flex-row">
-      {Array.from({ length: 5 }).map(
-        (_, i) =>
-          value !== undefined && (
-            <Ionicons
-              key={i}
-              name={i < Math.round(value) ? 'star' : 'star-outline'}
-              size={20}
-              color="#FFD700"
-              className="mx-1"
-            />
-          ),
-      )}
+      {Array.from({ length: 5 }).map((_, i) => {
+        if (value === undefined) return null;
+
+        let iconName: 'star-outline' | 'star' | 'star-half' = 'star-outline';
+        if (value >= i + 1) {
+          iconName = 'star';
+        } else if (value >= i + 0.5) {
+          iconName = 'star-half';
+        }
+
+        return (
+          <Ionicons
+            key={i}
+            name={iconName}
+            size={20}
+            color="#FFD700"
+            className="mx-1"
+          />
+        );
+      })}
     </View>
   );
 };
